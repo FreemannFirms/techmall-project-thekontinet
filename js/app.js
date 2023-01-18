ef6f53159755727e10d02349a3bb373651356fb4
rolomtech <support@rolomtech.com>
2023-01-18 13:08:24 +0100

    banner section completed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 const navEl = document.querySelector('.nav')
 const buttonEl = document.querySelector('.nav-toggle')
+const headerEl = document.querySelector('.header')
 
 buttonEl.addEventListener('click', function(){
     if(navEl.classList.contains('active')){
         navEl.classList.remove('active')
     }else{
         navEl.classList.add('active')
+    }
+})
+
+window.addEventListener('scroll', function(event){
+    if(window.scrollY >= 35){
+        headerEl.classList.add('scrolled')
+    }else{
+        headerEl.classList.remove('scrolled')
     }
 })
